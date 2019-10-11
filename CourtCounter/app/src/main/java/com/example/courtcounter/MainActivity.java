6e4fe39c78d0e0c3d8fe0385fc091e90c3f0291c
znannan <znannan@gmail.com>
2019-10-11 15:48:28 -0700

    some debug

---
+++ CourtCounter/app/src/main/java/com/example/courtcounter/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.courtcounter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+
+
+
+}
