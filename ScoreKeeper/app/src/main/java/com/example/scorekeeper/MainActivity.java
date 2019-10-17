2d856adb59343bceac24d03a072ec43ce7fe7e92
znannan <znannan@gmail.com>
2019-10-16 23:44:45 -0700

    project2 score keeper finished

--- ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
+++ ScoreKeeper/app/src/main/java/com/example/scorekeeper/MainActivity.java
@@ -4,12 +4,65 @@ package com.example.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    int teamAScore = 0;
+    int teamBScore = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void displayA(int teamAScore){
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(teamAScore));
+    }
+
+    public void displayB(int teamBScore){
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(teamAScore));
+    }
+
+    public void add6ForA(View v){
+        teamAScore += 6;
+        displayA(teamAScore);
+    }
+
+    public void add3ForA(View v){
+        teamAScore += 3;
+        displayA(teamAScore);
+    }
+
+    public void add2ForA(View v){
+        teamAScore += 2;
+        displayA(teamAScore);
+    }
+    public void add6ForB(View v){
+        teamAScore += 6;
+        displayA(teamAScore);
+    }
+
+    public void add3ForB(View v){
+        teamAScore += 3;
+        displayA(teamAScore);
+    }
+
+    public void add2ForB(View v){
+        teamAScore += 2;
+        displayA(teamAScore);
+    }
+
+    public void reSet(View v){
+        teamAScore = 0;
+        teamBScore = 0;
+        displayA(teamAScore);
+        displayB(teamBScore);
+    }
+
+
 }
