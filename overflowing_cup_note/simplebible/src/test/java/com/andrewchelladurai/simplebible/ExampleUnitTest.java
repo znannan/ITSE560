02f0abe9eb40df6af515ec7819310869503dabb5
znannan <znannan@gmail.com>
2019-12-26 02:00:50 -0800

    simple Bible app

---
+++ overflowing_cup_note/simplebible/src/test/java/com/andrewchelladurai/simplebible/ExampleUnitTest.java
@@ -0,0 +1,19 @@
+package com.andrewchelladurai.simplebible;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ * <p>
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+
+  @Test
+  public void addition_isCorrect() {
+    assertEquals(4, 2 + 2);
+  }
+
+}
